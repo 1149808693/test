2213f65d89a463f7d993dbec48ed6e3857d4d6c1
susui <18829343499@163.com>
2020-03-25 21:25:27 +0800

    修改了

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("111111");
+        System.out.println("222222");
         System.out.println("333333");
     }
 }
